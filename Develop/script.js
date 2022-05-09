feed9b932cc8abb0c805f2b8e9c80ce7fafcb0c8
Sean <seanmcdaniel93@gmail.com>
2022-05-08 22:06:49 -0400

    Finished Local Storage functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,24 @@
 $(document).ready(function () {
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
+    // Event listener for the "save" button
     $(".saveBtn").on("click", function () {
         console.log(this);
-        var text = $(this).siblings(".description").val(); 
+        var event = $(this).siblings(".description").val(); 
         var time = $(this).parent().attr("id");
 
-        localStorage.setItem(time, text);
+        localStorage.setItem(time, event);
     })
-    
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour1 .description").val(localStorage.getItem("hour1"));
-    $("#hour2 .description").val(localStorage.getItem("hour2"));
-    $("#hour3 .description").val(localStorage.getItem("hour3"));
-    $("#hour4 .description").val(localStorage.getItem("hour4"));
-    $("#hour5 .description").val(localStorage.getItem("hour5"));
-    $("#hour6 .description").val(localStorage.getItem("hour6"));
 
-    function hourTracker() {
+    // Declaring a function to tell the current hour using moment.js
+    function timeTracker() {
         var currentHour = moment().hour();
-
+        
+        // Function for looping over the time blocks
         $(".time-block").each(function () {
             var blockHour = parseInt($(this).attr("id").split("hour")[1]);
             console.log( blockHour, currentHour)
 
+            // Conditionals for changing background color of the time blocks
             if (blockHour < currentHour) {
                 $(this).addClass("past");
                 $(this).removeClass("future");
@@ -43,5 +36,19 @@ $(document).ready(function () {
             }
         })
     }
-    hourTracker();
+    
+    // Calling event items saved in local storage. You have to use military time. 
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
+
+    timeTracker();
 })
